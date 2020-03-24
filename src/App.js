d00568cc8fe01c43a3fc5f44c86ce1234d9823b4
Arnel Imperial <engine@Arnels-MacBook-Pro.local>
2020-03-24 23:59:00 +0200

    Added search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 //import logo from './logo.svg';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -8,7 +10,8 @@ class App extends Component {
     super();
 
     this.state = {
-      birls: []
+      birls: [],
+      searchFiled: ''
     };
   }
   // Life-cycle
@@ -20,9 +23,18 @@ class App extends Component {
   }
 
   render(){
+    const { birls, searchFiled } = this.state;
+    const filteredBirls = birls.filter(birl => 
+        birl.name.toLowerCase().includes(searchFiled.toLowerCase())
+      );
     return(
       <div className="App">
-        <CardList birls={ this.state.birls } />
+        
+        <SearchBox 
+          placeholder="Search name" 
+          handleChange={e => this.setState({ searchFiled: e.target.value })}
+        />
+        <CardList birls={ filteredBirls } />
        
       </div>
     );
