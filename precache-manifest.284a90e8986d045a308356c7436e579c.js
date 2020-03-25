19e77f58367de93fab020692da636c3981046b3b
Arnel Imperial <engine@Arnels-MacBook-Pro.local>
2020-03-25 16:39:53 +0200

    Updates

---
+++ precache-manifest.284a90e8986d045a308356c7436e579c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9441eb6b1a2848e44cc5cb8dd408d950",
+    "url": "/birl/index.html"
+  },
+  {
+    "revision": "58c73c8c1c3a6a6cac6e",
+    "url": "/birl/static/css/main.d5e65baa.chunk.css"
+  },
+  {
+    "revision": "a04efd0d023753295dce",
+    "url": "/birl/static/js/2.c3618d9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birl/static/js/2.c3618d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c73c8c1c3a6a6cac6e",
+    "url": "/birl/static/js/main.aa3b5d86.chunk.js"
+  },
+  {
+    "revision": "138df94d9a94443352c5",
+    "url": "/birl/static/js/runtime-main.2007e1e3.js"
+  }
+]);
